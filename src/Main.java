0c911976a00005cd9c24a6c6aef4ae8f9f82f577
am999 <am9999072080@gmail.com>
2023-05-07 18:01:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+public class Main {
+
+    private static final List <Integer> nums =new ArrayList<>(List.of(1,1,2,3,4,4,5,5,6,7));
+    private static final List <String> words =new ArrayList<>(List.of("abc", "abc", "abcd","abca", "abcd", "abca","abc"));
+    private static final List <String> words2 = new ArrayList<>(List.of("один", "один", "два","два", "два", "три", "три", "три"));
+
+    public static void main(String[] args) {
+
+    }
+}
